d07e6093ab5633ea2d63fecf00d5422f48fb4ea0
Mounikar99 <98777326+Mounikar99@users.noreply.github.com>
2025-09-26 12:39:43 -0400

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,9 @@
+age = 25             # int
+height = 5.3         # float
+name = "Mounika"     # string
+is_student = True    # boolean
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
